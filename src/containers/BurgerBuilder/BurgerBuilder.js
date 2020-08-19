b2ae5bbd423c71f6f2d87c5932fc8b147b93b47b
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-18 20:06:02 -0500

    add redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,8 +92,6 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = () => {
-		//alert('You continue!');
-
 		const queryParams = [];
 		for (let i in this.state.ingredients) {
 			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
