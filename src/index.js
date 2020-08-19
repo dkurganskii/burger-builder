028599ca4af9756542ce418254ff02a42ea4abd9
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-18 20:22:19 -0500

    redux setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './store/reducer';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware()));
 
 const app = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
