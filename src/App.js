6c928855a4e4275462bc0d336f155f2d4463f9dd
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-24 10:40:23 -0500

    fix the redirect to the frontpage

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
 	render() {
 		let routes = (
 			<Switch>
-				<Route path="/auth" component={Auth} />
 				<Route path="/" exact component={BurgerBuilder} />
+				<Route path="/auth" component={Auth} />
 				<Redirect to="/" />
 			</Switch>
 		);
@@ -28,6 +28,7 @@ class App extends Component {
 					<Route path="/checkout" component={Checkout} />
 					<Route path="/orders" component={Orders} />
 					<Route path="/logout" component={Logout} />
+					<Route path="/auth" component={Auth} />
 					<Route path="/" exact component={BurgerBuilder} />
 					<Redirect to="/" />
 				</Switch>
