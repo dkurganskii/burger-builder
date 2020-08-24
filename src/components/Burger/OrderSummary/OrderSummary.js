e20bf578c92faab5e424d0bef1237b6ef4c002dd
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-24 11:41:56 -0500

    remove console.log()s

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,9 +4,6 @@ import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-	componentDidUpdate() {
-		console.log('[Order Summary] Did Update!');
-	}
 	render() {
 		const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
 			return (
