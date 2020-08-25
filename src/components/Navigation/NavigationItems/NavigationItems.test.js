589b1aa48762e2c338b076e9d1037950b2ad1ca8
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-25 11:33:45 -0500

    testing components

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -19,14 +19,14 @@ describe('<NavigationItems />', () => {
 		expect(wrapper.find(NavigationItem)).toHaveLength(2);
 	});
 
-	//     it('should render three <NavigationItem /> elements if authenticated', () => {
-	//         // wrapper = shallow(<NavigationItems isAuthenticated />);
-	//         wrapper.setProps({isAuthenticated: true});
-	//         expect(wrapper.find(NavigationItem)).toHaveLength(3);
-	//     });
+	it('should render three <NavigationItem /> elements if authenticated', () => {
+		// wrapper = shallow(<NavigationItems isAuthenticated />);
+		wrapper.setProps({ isAuthenticated: true });
+		expect(wrapper.find(NavigationItem)).toHaveLength(3);
+	});
 
-	//     it('should an exact logout button', () => {
-	//         wrapper.setProps({isAuthenticated: true});
-	//         expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
-	//     });
+	it('should an exact logout button', () => {
+		wrapper.setProps({ isAuthenticated: true });
+		expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+	});
 });
